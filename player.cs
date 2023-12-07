1feb2baba242217c7d4c467d90281365743c3442
Gabriel Fernandes <gabrielsbfernandes@gmail.com>
2023-12-07 18:51:11 -0300

    New layers

--- player.cs
+++ player.cs
@@ -3,32 +3,35 @@ using System;
 
 public partial class player : Area2D
 {
-	[Signal]
-	public delegate void HitEventHandler();
 	
 	[Export]
-	public int Speed {get; set;} = 120;
+	public int Speed {get; set;} = 200;
 	public Vector2 ScreenSize;
-
 	public override void _Ready()
 	{
 		ScreenSize = GetViewportRect().Size;
 		//Hide();
 	}
+
 	public override void _Process(double delta)
 	{
+		
 		var velocity = Vector2.Zero; // The player's movement vector.
-
+		velocity.Y+=1;
 		if (Input.IsActionPressed("move_right"))
 		{
 			velocity.X += 1;
 		}
-
+		if (Input.IsActionPressed("Jump"))
+		{
+			velocity.Y -= 100;
+		}
 		if (Input.IsActionPressed("move_left"))
 		{
 			velocity.X -= 1;
 		}
-	
+		
+
 	   var animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
 
 	   if (velocity.Length() > 0)
@@ -36,10 +39,7 @@ public partial class player : Area2D
 			velocity = velocity.Normalized() * Speed;
 			animatedSprite2D.Play();
 	   }
-	//    else
-	//    {
-	// 	   animatedSprite2D.Stop();
-	//    }
+
 		Position += velocity * (float)delta;
 		Position = new Vector2(
 			x: Mathf.Clamp(Position.X, 0, ScreenSize.X),
@@ -61,23 +61,7 @@ public partial class player : Area2D
 		{
 			animatedSprite2D.FlipH = true;
 		}
-		// else
-		// {
-		// 	animatedSprite2D.FlipH = false;
-		// }
-		}
-		private void _on_body_entered(Node2D body)
-		{
-			Hide(); // Player disappears after being hit.
-			EmitSignal(SignalName.Hit);
-			// Must be deferred as we can't change physics properties on a physics callback.
-			GetNode<CollisionShape2D>("CollisionShape2D").SetDeferred(CollisionShape2D.PropertyName.Disabled, true);
-		}
-		public void Start(Vector2 position)
-		{
-			Position = position;
-			Show();
-			GetNode<CollisionShape2D>("CollisionShape2D").Disabled = false;
-		}
+	}
+		
 }
 
